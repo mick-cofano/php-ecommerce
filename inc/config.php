b8419cdc493f642d8b9ce06c251ee8b303c527d1
mick-cofano <brutalmachinelol@gmail.com>
2021-06-22 18:32:07 +0200

    Aggiunti prodotti tramite DB e php

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('ROOT_URL', 'http://localhost/php-ecommerce/');
+    define('ROOT_PATH', 'C:\\MAMP\\htdocs\\php-ecommerce\\');
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'phpecommerce');
--- shop/pages/products-list.php
+++ shop/pages/products-list.php
@@ -1,1 +1,28 @@
-hello from products-list.php
+<?php
+    // echo 'hello from "' . basename(__FILE__) .'"';
+
+    $productMgr = new ProductManager();
+    $products = $productMgr->getAll();
+    // $products = null;
+    // var_dump($products);
+?>
+
+
+<div class="row">
+    <?php if($products) : ?>
+        <?php foreach($products as $product) : ?>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $product->name ?></h5>
+                <h6 class="card-dubtitle mb-2 text-muted"><?php echo $product->price ?> €</h6>
+                <p class="card-text"><?php echo $product->description ?></p>
+                <a href="<?php echo ROOT_URL . 'shop?page=view-product&id=' . $product->id ?>" class="card-link">Vedi &raquo;</a>
+            </div>
+        </div>
+
+        <?php endforeach; ?>
+    <?php else : ?>
+        <p>Nessun prodotto disponibile</p>
+    <?php endif; ?>
+</div>
